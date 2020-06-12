7e5c6afa504358f7b32c599a1a24ad99dad79d7c
Nicholas Sylke <nicksylke@outlook.com>
2020-06-12 15:51:20 -0500

    feat: setup next.config.js

---
+++ next.config.js
@@ -0,0 +1,20 @@
+const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants');
+
+module.exports = (phase) => {
+    const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+    const isCanary = phase === PHASE_PRODUCTION_BUILD && process.env.CANARY === 'true';
+    const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.CANARY !== 'true';
+
+    const env = {
+        NEXT_API_ROUTE: (() => {
+            if (isProd) return 'https://typicalbot.com/api';
+            if (isCanary) return 'https://canary.typicalbot.com/api';
+            if (isDev) return 'http://localhost:3000/api';
+        })()
+    };
+
+    return {
+        env,
+        poweredByHeader: false
+    }
+}
