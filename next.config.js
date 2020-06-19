7542cb634657e528f36710ebfc966986ccd5bd3f
Nicholas Sylke <nicksylke@outlook.com>
2020-06-19 17:04:19 -0500

    feat: integrate sentry (#22)
    * feat: integrate sentry
    
    * chore: update NEXT_PUBLIC_SENTRY_DSN env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants');
+const withSourceMaps = require('@zeit/next-source-maps')();
+const SentryWebpackPlugin = require('@sentry/webpack-plugin');
 
 module.exports = (phase) => {
     const isDev = phase === PHASE_DEVELOPMENT_SERVER;
@@ -11,10 +13,11 @@ module.exports = (phase) => {
             if (isCanary) return 'https://canary.typicalbot.com/api';
             if (isDev) return 'http://localhost:3000/api';
         })(),
-        NEXT_PUBLIC_DISCORD_API: 'https://discord.com/api'
+        NEXT_PUBLIC_DISCORD_API: 'https://discord.com/api',
+        NEXT_PUBLIC_SENTRY_DSN: 'https://5348c51f82d945b398b47c0b640cfeff@o161567.ingest.sentry.io/5270885'
     };
 
-    return {
+    return withSourceMaps({
         env,
         poweredByHeader: false,
         experimental: {
@@ -43,6 +46,24 @@ module.exports = (phase) => {
                     }
                 ]
             }
+        },
+        webpack: (config, options) => {
+            if (!options.isServer) {
+                config.resolve.alias['@sentry/node'] = '@sentry/browser';
+            }
+
+            if (isProd) {
+                config.plugins.push(
+                    new SentryWebpackPlugin({
+                        include: '.next',
+                        ignore: ['node_modules'],
+                        urlPrefix: '~/_next',
+                        release: options.buildId
+                    })
+                );
+            }
+
+            return config;
         }
-    }
+    });
 }
---
+++ pages/_error.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import NextErrorComponent, { ErrorProps } from 'next/error';
+import * as Sentry from '@sentry/node';
+
+// @ts-ignore
+const MyError = ({ statusCode, hasGetInitialPropsRun, err }: ErrorProps) => {
+    if (!hasGetInitialPropsRun && err) {
+        Sentry.captureException(err);
+    }
+
+    return <NextErrorComponent statusCode={statusCode} />
+}
+
+// @ts-ignore
+MyError.getInitialProps = async ({ res, err, asPath }) => {
+    // @ts-ignore
+    const errorInitialProps = await NextErrorComponent.getInitialProps({ res, err });
+
+    // @ts-ignore
+    errorInitialProps.hasGetInitialPropsRun = true;
+
+    if (res?.statusCode === 404) {
+        return {
+            statusCode: 404
+        }
+    }
+
+    if (err) {
+        Sentry.captureException(err);
+        return errorInitialProps;
+    }
+
+    Sentry.captureException(
+        new Error(`_error.js getInitialProps missing data at path: ${asPath}`)
+    );
+
+    return errorInitialProps;
+}
+
+export default MyError;
