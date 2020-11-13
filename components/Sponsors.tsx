c3ef961d0e947c4b998282c4489d53b71ae3ea61
Nicholas Sylke <nicksylke@outlook.com>
2020-11-13 17:53:39 -0600

    feat: sponsor jetbrains/crowdin/discordservers at bottom of homepage

---
+++ components/Sponsors.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+const Sponsors: React.FC = () => (
+    <React.Fragment>
+        <div className="container">
+            <div className="sponsors">
+                <div className="sponsor">
+                    <a href="https://www.jetbrains.com/">
+                        <img src="/img/jb-logo.png" alt="Jetbrains" height="100px"/>
+                    </a>
+                </div>
+                <div className="sponsor">
+                    <a href="https://crowdin.com/">
+                        <img src="/img/cin-logo.png" alt="Crowdin" height="100px"/>
+                    </a>
+                </div>
+                <div className="sponsor">
+                    <a href="https://discordservers.me/">
+                        <img src="/img/ds-logo.png" alt="Discord Servers" height="80px" className="invert"/>
+                    </a>
+                </div>
+            </div>
+        </div>
+        {/* language=CSS */}
+        <style jsx>{`
+            .container {
+                max-width: 1280px;
+                display: flex;
+                flex-direction: column;
+                margin: 0 auto;
+                padding: 0 40px;
+            }
+
+            .sponsors {
+                display: grid;
+                grid-template-columns: 1fr 1fr 1fr;
+                grid-column-gap: 1rem;
+                padding: 120px 0 0;
+            }
+
+            .sponsor {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+            .sponsor img.invert {
+                filter: invert(100%);
+            }
+        `}</style>
+    </React.Fragment>
+)
+
+export default Sponsors;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {NextPage} from 'next';
 import Layout from 'components/Layout';
 import Hero from 'components/Hero';
+import Sponsors from "../components/Sponsors";
 
 const Page: NextPage = () => (
     <React.Fragment>
         <Layout title="Home">
             <Hero/>
+            <Sponsors/>
         </Layout>
         {/* language=CSS */}
         <style jsx global>{`
